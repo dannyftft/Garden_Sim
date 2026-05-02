c630bc485b978403e54b20ae224377b82997519b
daniel <daniel.ladkin@yandex.com>
2026-05-02 18:50:30 +0200

    Player class implemented

--- src/game/Player.java
+++ src/game/Player.java
@@ -5,19 +5,26 @@ public class Player {
     private int money;
 
     public Player(int startingMoney) {
+        this.money = startingMoney; // sets the starting balance
     }
 
     public int getMoney() {
-        return 0;
+        return money;
     }
 
     public void setMoney(int money) {
+        this.money = money;
     }
 
     public void addMoney(int amount) {
+        this.money = this.money + amount; // adds to the current balance when selling a plant
     }
 
     public boolean spendMoney(int amount) {
-        return false;
+        if (amount > money) {
+            return false; // not enough money, purchase blocked
+        }
+        this.money = this.money - amount; // deducts the cost from the balance
+        return true; // purchase went through
     }
 }
